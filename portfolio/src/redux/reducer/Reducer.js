98cccbfb14ccd2fcdfd7f77f210d93a0b053bed7
amaljithmk07 <amaljithmk123@gmail.com>
2024-04-26 15:26:33 +0530

    darkmode

--- portfolio/src/redux/reducer/Reducer.js
+++ portfolio/src/redux/reducer/Reducer.js
@@ -2,7 +2,7 @@ import { createSlice } from "@reduxjs/toolkit";
 export const DarkmodeSlice = createSlice({
   name: "Darkmodestate",
   initialState: {
-    value: false,
+    value: true,
   },
   reducers: {
     DarkmodeHandler: (state, action) => {
